7d7b987409d81a7b0949e0effe2038e4cd0b3707
E14ndx <tonitolepori@gmail.com>
2024-09-29 18:40:04 -0300

    cambios en urls
    la pgina principal dira hola

--- Proyecto_intro/urls.py
+++ Proyecto_intro/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Proyecto_intro import introduccion
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("hola/", introduccion)
 ]
