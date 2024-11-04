1c8150551dcb2015df79ae4a348ad4cf3e7c43ed
E14ndx <tonitolepori@gmail.com>
2024-11-04 20:22:21 -0300

    Login
    Login "funcional"

--- Proyecto_intro/urls.py
+++ Proyecto_intro/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from Proyecto_intro.Views import introduccion
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("introduccion/", introduccion)
+    path("introduccion/", introduccion),
+    path('db/', include('db.urls'))
 ]
--- db/views.py
+++ db/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
-# Create your views here.
+# Vista para el registro
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirigir al login después de registrarse
+    else:
+        form = UserCreationForm()
+    return render(request, 'db/register.html', {'form': form})
+
+# Vista para el login
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Redirigir al home después de hacer login
+    else:
+        form = AuthenticationForm()
+    return render(request, 'db/login.html', {'form': form})
